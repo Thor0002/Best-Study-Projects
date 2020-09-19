740ab62fcbc065c2b4e6f14abdf335c55bcb5486
Kirill Selivanov <tor20005@mail.ru>
2020-09-19 11:34:48 +0500

    Update Parsing file by function.rkt

--- Parsing file by function/Parsing file by function.rkt
+++ Parsing file by function/Parsing file by function.rkt
@@ -1,7 +1,7 @@
 #lang scheme
 
-; Given the text of the program in Scheme given at the input (read from the input file), 
-; the function ParseFile outputs the names of the functions described in this file into two output files:
+; Using the input text of the Scheme program (the file name is read from the input file), 
+; the function ParseFile outputs the names of the functions described in this file to two output files:
 ; the first is non-recursive functions("non recursive functions.txt"), 
 ; the second is recursive functions("recursive functions.txt").
 
